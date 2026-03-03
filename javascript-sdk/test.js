3a83f51175f5ac1c0aeb3b21ebc1d0ee0d38f233
Friction <freshwolf4@gmail.com>
2026-03-04 01:06:22 +0530

    js updated

--- javascript-sdk/test.js
+++ javascript-sdk/test.js
@@ -1,7 +1,9 @@
 const { AquinClient } = require("./dist/index.js");
 
 const client = new AquinClient("aq-m_3pAhhPRqjv-XWdlY8SFZ7L10XztsysBaqcg-ThzjBkg");
-client.complete("tell me about exdensur").then(res => {
+client.complete("What conditions exdensur should be used for?", {
+  system_prompt: "You are a knowledgeable medical information specialist for Exdensur."
+}).then(res => {
   console.log(res.text);
   console.log(`tokens_in=${res.tokens_in} tokens_out=${res.tokens_out}`);
 });
